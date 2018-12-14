b92b8b8bd5184347970844b4f1cef6cc014e1aad
josemanuel.almaza <josemanuel.almaza@by.com.es>
2018-12-14 13:27:31 +0100

    created client

--- src/client/main.go
+++ src/client/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	config "config/client"
 	"fmt"
+
+	"github.com/go-resty/resty"
 )
 
 func main() {
 	config := config.NewCliConfig()
-
 	fmt.Println(config.ServerIp())
+
+	resp, err := resty.R().SetHeader("Content-Type", "application/json").
+		Post("http://" + config.ServerIp() + ":8000/games")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(resp)
 }
